b20f1c51a2a4a27df393a6f6e5848aadd9fb43ab
narasaki ryosei <gennarikun@gmail.com>
2023-04-03 13:21:03 +0900

    fix:ファイル修正

--- js/index.js
+++ js/index.js
@@ -1,11 +1,3 @@
 ((d, w) => {
-  /**
-    アコーディオン
-  */
-  const headline = d.querySelectorAll('.js-accordion_headline');
-  for (let i = 0; i < headline.length; i++) {
-    headline[i].addEventListener('click', (e) => {
-      e.target.parentNode.classList.toggle('is-accordion_item');
-    });
-  }
+  
 })(document, window);
